60c55057968bfff9ddac1b7387aa474ef26d489f
ds10125 <ds11705@hotmail.com>
2018-11-20 14:00:05 +0800

    Create Arrays - DS.js

---
+++ Arrays - DS.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the reverseArray function below.
+function reverseArray(a) {
+
+    var newArr = []; 
+    if (a.length == 0) {
+        return 0;
+    } else if (a.length == 1) {
+        return a[1];
+    }
+    for (var x = 0; x < a.length; x++) {
+        newArr[x] = a[a.length - 1 - x];
+
+    } 
+    return newArr;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
