fac856daff5d8db60ce56c140c7e4b41cb1484aa
ds10125 <ds11705@hotmail.com>
2018-11-20 15:28:55 +0800

    Create Big Sorting.js
    Sample Input 1
    
    8
    1
    2
    100
    12303479849857341718340192371
    3084193741082937
    3084193741082938
    111
    200
    Sample Output 1
    
    1
    2
    100
    111
    200
    3084193741082937
    3084193741082938
    12303479849857341718340192371

---
+++ Big Sorting.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bigSorting function below.
+function bigSorting(unsorted) {
+    unsorted.sort(function (a, b) {
+        if (a.length == b.length) {
+            return a > b ? 1 : -1;
+        }
+        return a.length - b.length;
+    });
+    return unsorted;
+}
+
+ 
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let unsorted = [];
+
+    for (let i = 0; i < n; i++) {
+        const unsortedItem = readLine();
+        unsorted.push(unsortedItem);
+    }
+
+    let result = bigSorting(unsorted);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
